9d99e16ad32086ac542897111201af5e88366d93
Danend <lenovoipslim5i@gmail.com>
2024-10-30 10:25:41 +0700

    Progress1

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.base')
+@section('content')
+    <h1>Dashboard Admin</h1>
+@endsection
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.base')
+@section('content')
+    <h1>Dashboard User</h1>
+@endsection
